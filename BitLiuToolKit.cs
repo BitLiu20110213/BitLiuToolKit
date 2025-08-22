33a470b66c41400c4ab99cc17aac67bbc08003b3
BitLiu <114805936+BitLiu20110213@users.noreply.github.com>
2025-08-22 17:12:38 +0800

    Upload Basic files
    Upload Basic files.

---
+++ BitLiuToolKit.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Hacknet;
+using Pathfinder.Executable;
+using Pathfinder.Port;
+using System;
+
+namespace BitLiuToolKit
+{
+    [BepInPlugin(ModGUID, ModName, ModVer)]
+    public class BitLiuToolKit : HacknetPlugin
+    {
+        public const string ModGUID = "com.BitLiu.BitLiuToolKit";
+        public const string ModName = "BitLiuToolKit";
+        public const string ModVer = "0.0.1";
+
+        public override bool Load()
+        {
+            string str = @"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#               BitLiuToolKit 0.0.1:A useful Mod                  #
+#                     BitLiu  (C)  2025                           #
+#           The Tool Kit is especially made for you.              #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(str);
+            Console.ResetColor();
+            this.LogDebug("Loading Executables...");
+            ExecutableManager.RegisterExecutable<BitLiuTool>("#BITLIU_TOOL#");
+            PortManager.RegisterPort("BitLiuPort", "BitLiu Backdoor", 213);
+            return true;
+        }
+
+        private void LogDebug(string message)
+        {
+            Log.LogDebug(message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# BitLiuToolKit
-一个有用的Hacknet Pathfinder Mod.
+BitLiuToolKit - 一个有用的[Hacknet](https://store.steampowered.com/app/365450/Hacknet/) [Pathfinder](https://github.com/Arkhist/Hacknet-Pathfinder)模组
+===
+
+如果你遇到了Bug或者有什么改进的想法, 欢迎发邮件至2856255083@qq.com!
+
+##  新的`Executibles`
+每个exe都有重做的图像与新机制:
+- `#BITLIU_TOOL#`|`BitLiuTool.exe`:破解`213 (BitLiu Backdoor)`端口
+  - 破解时间大概25秒;
+  - 会额外开放188，211，500端口(端口详见ZeroDayToolKit和XMOD);
+- 等待更新
+
+## 新的`Action`
+- 等待更新
+
+## 新的`Daemon`
+- 等待更新
+
+## 新的`Goals`
+- 等待更新
